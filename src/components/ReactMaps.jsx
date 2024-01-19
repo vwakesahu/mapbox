47e40643f5bc2e4838e0b0632c828aeb3d7d3a3b
Vivek Sahu <viveksahu1762@gmail.com>
2024-01-19 12:41:18 +0530

    Gets and marks the current location on Map using MapBox

---
+++ src/components/ReactMaps.jsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Head from "next/head";
+import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+export default function ReactMaps() {
+  const [currentPosition, setCurrentPosition] = React.useState({
+    latitude: null,
+    longitude: null,
+  });
+
+  React.useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setCurrentPosition({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    } else {
+      console.log("Geolocation is not available in your browser.");
+    }
+  }, []);
+
+  return (
+    <div>
+      {currentPosition.latitude != null && currentPosition.longitude != null && (
+        <Map
+          initialViewState={{
+            latitude: currentPosition.latitude,
+            longitude: currentPosition.longitude,
+            zoom: 14,
+          }}
+          style={{ width: "100vw", height: "100vh" }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        >
+          
+
+          <Marker
+            longitude={currentPosition.longitude}
+            latitude={currentPosition.latitude}
+            color="red"
+          />
+        </Map>
+      )}
+    </div>
+  );
+}
